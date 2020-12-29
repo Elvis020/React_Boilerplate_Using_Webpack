e3cb857265bd11dfe914eb33e2f48ce3e7db8aac
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-12-29 09:21:56 +0000

    Added webpack files

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import './styles/style.css';
+
+const App = () => {
+  return (
+    <h1>Hello sPaCeY 🚶🚀</h1>
+  )
+}
+
+reactDOM.render(<App />, document.getElementById('root'))
